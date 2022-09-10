357ade88b69740841020a1b3ab040f381cff7ba3
antonmax29 <antssilva1992@gmail.com>
2022-09-10 17:37:09 -0300

    commit btn dec/inc interativo finalizado

--- carrinho.js
+++ carrinho.js
@@ -4,6 +4,78 @@ const total = document.getElementById('total')
 
 const botao_enviar= document.getElementById('btn-enviar')
 
+const btn_menos = document.getElementsByClassName('btn-subtrair')
+
+const btn_adicionar = document.getElementsByClassName('btn-adicionar')
+
+const input = document.getElementsByClassName('quantity');
+/*var x = 1*/
+//btn_menos[0].addEventListener('click', subInput)
+//btn_adicionar[0].addEventListener('click', addInput)
+function addInput(value) {
+
+    
+      /*  const inputMais = produtos[0].getElementsByClassName('btn-adicionar')
+        var entrada = inputMais[0].previousSibling.value 
+
+    
+        
+        var valor = parseFloat(entrada)
+        valor++
+        document.querySelector('.btn-adicionar').previousSibling.value = valor
+        calculaTotal()
+     
+        console.log(entrada)*/
+        for (var index = 0; index < produtos.length; index++) {
+            const btnmais = produtos[index].getElementsByClassName('quantity')
+            if(value == (index+1)){
+            var x = btnmais[0].value
+            x++
+            btnmais[0].value = x
+            calculaTotal()
+        }
+            //input[0].setAttribute('id', `${index+1}`)
+        }
+    
+   /* var element = document.querySelector('.btn-adicionar').previousSibling.value;
+    var x = parseFloat(element)
+    x++
+    document.querySelector('.btn-adicionar').previousSibling.value = x
+    calculaTotal()
+
+    //console.log(btn_menos)*/
+}
+
+function subInput(value) {
+//const valores = document.querySelector('button input')
+/*const inputMenos = produtos[0].getElementsByClassName('btn-subtrair')
+var entrada1 = inputMenos[0].nextElementSibling.value 
+
+
+
+var valor1 = parseFloat(entrada1)
+valor1--
+document.querySelector('.btn-subtrair').nextElementSibling.value = valor1
+calculaTotal()*/
+
+for (var index = 0; index < produtos.length; index++) {
+    const btnmenos = produtos[index].getElementsByClassName('quantity')
+    if(value == (index+1)){
+    var b = btnmenos[0].value
+    b--
+    btnmenos[0].value = b
+    calculaTotal()
+}}
+   /* var y = document.querySelector('.btn-subtrair').nextElementSibling.value;
+
+   var x = parseFloat(y)
+    x--
+    document.querySelector('.btn-subtrair').nextElementSibling.value = x
+    calculaTotal()*/
+   
+}
+
+
 window.onchange = calculaTotal
 
 function calculaTotal() {
@@ -11,6 +83,9 @@ function calculaTotal() {
 
 
 for (var index = 0; index < produtos.length; index++) {
+    const input = produtos[index].getElementsByClassName('quantity')
+    input[0].setAttribute('id', `${index+1}`)
+
     const elementsPrice = produtos[index].getElementsByClassName('preco');
     const priceText = elementsPrice[0].innerHTML
     const price = converteTextNumber(priceText)
